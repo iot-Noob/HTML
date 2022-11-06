50e1c6702c340e30852c7dba26a7208a31276567
Noob Programmer <38003373+iot-Noob@users.noreply.github.com>
2022-11-06 08:29:13 -0800

    Editable Dynamic Datatables
    Data tables where users can edit and delete data from tables. update of previous

---
+++ Dunamic table with edit delete using data tables/main.js
@@ -0,0 +1,126 @@
+var index=0;
+var fn,ln,age,cid;
+var nm,lnm,ags ,_tr ,_id;
+$("#fnr").hide();
+$("#lnr").hide();
+$("#ar").hide();
+
+$(document).ready(function(){
+  var cf=  $('#mtb').DataTable();
+    responsive:true
+ 
+$("#bta").on('click',function(){
+    
+ fn=$("#Fname").val();
+ ln=$("#Lname").val();
+ age=$("#age").val();
+ 
+
+
+if(fn==""&&ln==""&&age==""||fn==""||ln==""||age=="") {
+ 
+    if(!fn==""&&!ln==""&&!age==""){
+        
+        $("#fnr").hide();
+        $("#lnr").hide();
+        $("#ar").hide();
+
+
+      }else{
+
+        if(fn==""){
+            $("#fnr").show();
+          }else{
+              $("#fnr").hide();
+          } 
+          if(ln==""){
+              $("#lnr").show();
+            }else{
+                $("#lnr").hide();
+            }
+      
+            if(age==""){
+              $("#ar").show();
+            }else{
+                $("#ar").hide();
+            }
+
+      }
+
+ 
+
+    
+
+}else{
+    // var dtc=`<tr  id="tdr${++index}"> <td>${index}</td> <td>${fn}</td> <td>${ln}</td> <td>${age}</td> <td> <button id="btd" name="bta" type="button" class="btn   btcs"> <span class="material-icons btcs">
+    // delete
+    // </span>
+    //  </button>  <button id="bte" name="bte" type="button" class="btn   btcs"> <span class="material-icons btcs">
+    //  edit
+    //  </span> </button> </td> </tr> `;
+
+      
+   var btdf=` <button id="btd" name="bta" type="button" class="btn  material-icons  btcs">delete</button>   <button id="bte" name="bte" type="button" class="btn material-icons  btcs">edit</button>`;
+     var udt={
+     id:index,
+     fname:fn,
+     lname:ln,
+     agf:age,
+     bte:btdf ,
+      
+     };
+    cf.row.add([udt.id,udt.fname,udt.lname,udt.agf,udt.bte]).draw(false);
+     //$('#mtb').append(dtc);
+    $("#Fname").val("");
+    $("#Lname").val("");
+    $("#age").val("");
+    index++;
+}
+
+});   
+
+$("#mtb").on('click','#btd',function(){ //remmove
+ 
+    // $(this).closest('tr').remove();
+    cf.row($(this).parents('tr')).remove().draw(false);
+     index--;
+});
+
+
+$("#mtb").on('click','#bte',function(){ //edit
+   
+ 
+ _tr=$(this).closest('tr');
+  _id=$(_tr).find('td:eq(0)').text();
+  cid=_id;
+
+  nm=$(_tr).find('td:eq(1)').text();
+ lnm=$(_tr).find('td:eq(2)').text();
+ ags=$(_tr).find('td:eq(3)').text();
+ $("#Fname").val(nm);
+ $("#Lname").val(lnm);
+ $("#age").val(ags);
+
+
+
+
+});
+
+$("#btu").click(function(){ //update button
+ 
+    fn=$("#Fname").val();
+    ln=$("#Lname").val();
+    age=$("#age").val();
+   
+ update_table();
+
+});
+
+});
+
+async function update_table(){
+// alert("Updated!!");
+$(_tr).find('td:eq(1)').text(fn);
+$(_tr).find('td:eq(2)').text(ln);
+$(_tr).find('td:eq(3)').text(age);
+}
